598b19f84f677e168b4f291c0d04ed9f80b5b994
kevinh-a <kevin.cong.ha@gmail.com>
2024-02-11 23:28:22 -0800

    Same name change for pausemenus

--- GDIM32_GroupProject/Assets/Scripts/PauseMenuAlternative.cs
+++ GDIM32_GroupProject/Assets/Scripts/PauseMenuAlternative.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenuAlternative : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
