f40e83e84445ef7b0987209bbb50b74dfe6e3243
jasmine1049 <jasmic16@uci.edu>
2024-02-29 19:30:19 -0800

    rotation skill??

--- GDIM32_GroupProject/Assets/Scripts/Spells/RotateSpell.cs
+++ GDIM32_GroupProject/Assets/Scripts/Spells/RotateSpell.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class RotateSpell : BaseSpell
 {
     private GameObject NewSpell;
+    //public GameObject wayPoint;
+    //private float timer = 0.5f;
     [SerializeField] private Transform rotateAround;
     [SerializeField] protected Rigidbody2D rb;
-    private float RotationSpeed = 45f;
+    private float RotationSpeed = 100f;
     private float uptime = 0;
 
     public override void DoSpell()
@@ -19,17 +21,35 @@ public class RotateSpell : BaseSpell
 
     void Update()
     {
-        this.transform.RotateAround(rotateAround.position, Vector3.forward, RotationSpeed * Time.deltaTime );
+        this.transform.RotateAround(rotateAround.position, Vector3.forward, RotationSpeed * Time.deltaTime);
 
         uptime += Time.deltaTime;
         //Debug.Log(uptime);
 
-        if(uptime >= 5)
+        if(uptime >= 10)
         {
             Debug.Log("nothing hit...?");
             Destroy(gameObject);
             uptime = 0;
         }
+
+        /*if(timer > 0)
+        {
+          timer -= Time.deltaTime;
+        }
+         if(timer <= 0)
+        {
+          //The position of the waypoint will update to the player's position
+          UpdatePosition();
+          timer = 0.5f;
+        }
+
+    void UpdatePosition()
+    {
+     wayPoint.transform.position = transform.position;
     }
+    */
+    }
+    
 }
 
